3db987b3b05a802581109f08bc1aa035776257df
Shelly T <ixchell.tingling@gmail.com>
2018-03-29 16:29:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ function nowServing(katzDeliLine) {
     }
   }
 }
+function currentLine(katzDeliLine) {
+var lineOrder = [];
+
+if (katzDeliLine.length === 0) {
+return "The line is curently empty." ;
+}
+
+for (var i = 0; i < katzDeliLine.length; i += 1) {
+  var name = katzDeliLine[i];
+ lineOrder.push(' ' + (i + 1) + '. ' + name);
+}
+return "the line is currently:" + lineOrder;
+}
